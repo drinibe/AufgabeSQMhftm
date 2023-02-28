305905e7e4619589c04a5fa7fe81cd4e33365af1
drinibe <valdrin.daki@gmx.ch>
2023-02-28 18:18:14 +0100

    Test4

--- src/test/java/ch/hftm/rechner/CalculatorTest.java
+++ src/test/java/ch/hftm/rechner/CalculatorTest.java
@@ -15,7 +15,7 @@ public class CalculatorTest {
 
 	@Test
 	public void addPositiveIntegers() {
-		double result = calculator.add(5, 3);
+		double result = calculator.add(5, 4);
 		Assertions.assertEquals(7, result, 0);
 	}
 
